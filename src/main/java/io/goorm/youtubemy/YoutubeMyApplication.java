4c1a1349a54863f0d3cb1aa294cb7b1b964109fc
yunrry <yunryy329@gmail.com>
2025-02-10 16:54:13 +0900

    springbootsetting

---
+++ src/main/java/io/goorm/youtubemy/YoutubeMyApplication.java
@@ -0,0 +1,13 @@
+package io.goorm.youtubemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeMyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoutubeMyApplication.class, args);
+    }
+
+}
