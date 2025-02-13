fb584d83a1bd4b7419943e51608068c3661a1959
yunrry <yunryy329@gmail.com>
2025-02-13 17:43:42 +0900

    MemberContoller

--- src/main/java/io/goorm/youtubemy/admin/controller/MemberController.java
+++ src/main/java/io/goorm/youtubemy/admin/controller/MemberController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -17,8 +18,9 @@ public class MemberController {
         return "admin/members/list";
     }
 
-    @GetMapping("/members/{membersId}")
-    public String view(Model model) {
+    @GetMapping("/members/{memberSeq}")
+    public String view(@PathVariable Long memberSeq, Model model) {
+
         return "admin/members/view";
     }
 }
