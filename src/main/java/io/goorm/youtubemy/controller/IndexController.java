6e1c5ff7f0fa66388594db34963ca99b065869db
yunrry <yunryy329@gmail.com>
2025-02-13 17:40:04 +0900

    index

--- src/main/java/io/goorm/youtubemy/controller/IndexController.java
+++ src/main/java/io/goorm/youtubemy/controller/IndexController.java
@@ -2,10 +2,16 @@ package io.goorm.youtubemy.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Slf4j
 @Controller
 public class IndexController {
-    
+
+    @GetMapping("/")
+    public String index(Model model) {
+        return "index";
+    }
 }
